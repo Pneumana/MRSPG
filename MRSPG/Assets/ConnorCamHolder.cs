8cfed929a8e9d8240da9622bca01255b86393b07
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-01-22 14:35:03 -0800

    Cam holder script

--- MRSPG/Assets/ConnorCamHolder.cs
+++ MRSPG/Assets/ConnorCamHolder.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ConnorCamHolder : MonoBehaviour
 {
+    public GameObject player;
+
+    public float maxCameraDist;
+
+    public float backstep;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +17,25 @@ public class ConnorCamHolder : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
+        RaycastHit hit;
+        Physics.Raycast(player.transform.position, -Camera.main.gameObject.transform.forward, out hit, maxCameraDist);
+        Debug.DrawRay(player.transform.position, -Camera.main.gameObject.transform.forward, Color.red, Time.deltaTime);
+        Debug.Log(hit.point);
+        if(hit.point == Vector3.zero)
+        {
+            transform.position = player.transform.position - Camera.main.gameObject.transform.forward * maxCameraDist;
+        }
+        else
+        {
+            transform.position = hit.point + (Camera.main.gameObject.transform.forward * backstep);
+        }
+
+
+        //raycast backwards from the player object
+
+        //set this to the raycast position - some distance idk so it doesnt clip into walls and stuff
 
         //transform.position =
     }
