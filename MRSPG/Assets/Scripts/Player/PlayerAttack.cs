0470dbf80623173a036c00cde67b24888d85f75b
Topapopato <114021549+Topapopato@users.noreply.github.com>
2024-03-04 14:45:26 -0800

    Melee updates (Minor visual bug)
    - Player faces enemy before attacking
    - Melee collider checked for enemies before sliding player forward
    - Minor visual bug caused by rotation, shouldn't affect gameplay

--- MRSPG/Assets/Scripts/Player/PlayerAttack.cs
+++ MRSPG/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,12 +10,14 @@ public class PlayerAttack : MonoBehaviour
     public int MeleeCombo; //Will be out of date if read by other scripts
     public int RecentAttack;
     public bool DealtDamage;
+    public bool ToggleVisualBug; //To prevent possible side effects, will be removed when bug fixed
     public GameObject AttackHitbox;
     private Metronome Metronome;
     private MeleeHitbox MeleeHitbox;
     public Controller control;
     private InputControls inputControls;
     private LockOnSystem lockOnSystem;
+    private GameObject Player;
     #endregion
     void Start()
     {
@@ -22,6 +25,7 @@ public class PlayerAttack : MonoBehaviour
         MeleeHitbox = GameObject.Find("MeleeHitbox").GetComponent<MeleeHitbox>();
         inputControls = GameObject.Find("Player").GetComponent<InputControls>();
         lockOnSystem = GameObject.Find("TimeScaler").GetComponent<LockOnSystem>();
+        Player = GameObject.Find("PlayerObj");
         MeleeCombo = 0;
     }
     public void Attack(InputAction.CallbackContext context) //Starts melee attack and updates melee combo
@@ -35,23 +39,30 @@ public class PlayerAttack : MonoBehaviour
         {
             MeleeCombo++;
         }
-        //inputControls.moveDirection = lockOnSystem.trackedEnemy.transform.position - transform.position;
+        if (lockOnSystem.trackedEnemy != null && ToggleVisualBug) 
+        { 
+            Vector3 EnemyDirection = (lockOnSystem.trackedEnemy.transform.position - Player.transform.position).normalized;
+            Player.transform.forward = new Vector3(EnemyDirection.x, 0, EnemyDirection.z);
+        }
         switch (MeleeCombo)
         {
-            default:
-                break;
             case 1:
-                inputControls.AddPush(inputControls.moveDirection, 30, 0.98f);
+                if (!EnemyInRange()) { inputControls.AddPush(Player.transform.forward, 30, 0.98f); }
                 break;
             case 2:
-                inputControls.AddPush(inputControls.moveDirection, 30, 0.98f);
+                if (!EnemyInRange()) { inputControls.AddPush(Player.transform.forward, 30, 0.98f); }
                 break;
             case 3:
-                inputControls.AddPush(inputControls.moveDirection, 30, 0.98f);
+                if (!EnemyInRange()) { inputControls.AddPush(Player.transform.forward, 30, 0.98f); }
                 break;
         }
         DealtDamage = false;
         RecentAttack = Metronome.BeatsPassed;
         StartCoroutine(MeleeHitbox.MeleeAttack(MeleeCombo));
+    }
+
+    public bool EnemyInRange()
+    {
+        return Physics.CheckBox(MeleeHitbox.transform.position, new Vector3(1.7f, 1.4f, 1), MeleeHitbox.transform.rotation, inputControls.enemyLayer);//hitbox size value temporarily hard-coded for testing, if hitbox size changes this will be inaccurate.
     }
 }
