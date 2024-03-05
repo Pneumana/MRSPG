04a177efca759adce28f76c0f8e9107fd8c857c7
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-03-04 20:46:09 -0800

    Lock on camera fix

--- MRSPG/Assets/Scripts/Player/HitscanBullet.cs
+++ MRSPG/Assets/Scripts/Player/HitscanBullet.cs
@@ -35,9 +35,10 @@ public class HitscanBullet : MonoBehaviour
             {
                 Debug.Log("Hit enemy x " + enemyCast.Length + " " + enemyCast[0].collider.gameObject.name, enemyCast[0].collider.gameObject);
                 Destroy(enemyCast[0].collider.gameObject);
-                Debug.DrawLine(transform.position, groundCast.point, Color.red, 10);
+                
                 foreach (RaycastHit hit in enemyCast)
                 {
+                    Debug.DrawLine(transform.position, hit.point, Color.red, 10);
                     if (hit.collider != null)
                     {
                         var AI = hit.collider.gameObject.GetComponent<CircleStrafePlayerAI>();
@@ -58,6 +59,34 @@ public class HitscanBullet : MonoBehaviour
             
             //Debug.Log("Hit ground");
             //Debug.DrawLine(transform.position, groundCast.point, Color.blue, 10);
+        }
+        else
+        {
+            GetComponent<LineRenderer>().SetPosition(0, transform.position);
+            GetComponent<LineRenderer>().SetPosition(1, transform.position + (transform.forward * 9999));
+            //Debug.DrawLine(transform.position, groundCast.point, Color.blue, 10);
+            enemyCast = Physics.RaycastAll(transform.position, transform.forward, 9999, LayerMask.GetMask("Enemy"));
+            if (enemyCast.Length > 0)
+            {
+                Debug.Log("Hit enemy x " + enemyCast.Length + " " + enemyCast[0].collider.gameObject.name, enemyCast[0].collider.gameObject);
+                Destroy(enemyCast[0].collider.gameObject);
+
+                foreach (RaycastHit hit in enemyCast)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red, 10);
+                    if (hit.collider != null)
+                    {
+                        var AI = hit.collider.gameObject.GetComponent<CircleStrafePlayerAI>();
+                        if (AI != null)
+                        {
+                            AI.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+                            AI.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(transform.forward.x, 0.1f, transform.forward.z) * 15, ForceMode.Impulse);
+                        }
+
+
+                    }
+                }
+            }
         }
         //Destroy(gameObject, 10f);
         StartCoroutine(Dissipate());
