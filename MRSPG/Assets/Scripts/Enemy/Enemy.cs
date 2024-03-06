056e5f1de4900be6959354545cf163da39ab873b
Christopher Volkmar <christopher_volkmar1@my.vcccd.edu>
2024-03-06 12:09:05 -0800

    subtle changes to enemies

--- MRSPG/Assets/Scripts/Enemy/Enemy.cs
+++ MRSPG/Assets/Scripts/Enemy/Enemy.cs
@@ -40,7 +40,8 @@ public class Enemy : MonoBehaviour
     bool playerInRange;
     bool ShootingRange;
     public bool PlayerIsInSight;
-
+    bool aggro = false;
+    LayerMask GroundMask;
     #endregion
 
     #region Define Enemy
@@ -70,6 +71,7 @@ public class Enemy : MonoBehaviour
                 boss_enemy = _enemy as BossEnemySettings;
                 gameObject.name = "Boss";
                 gameObject.tag = "Enemy";
+                Rigidbody = gameObject.GetComponent<Rigidbody>();
                 break;
 
         }
@@ -129,7 +131,8 @@ public class Enemy : MonoBehaviour
     {
         //Use the SetEnemyData(EnemySetting _enemy) function to use the correct variables.
         SetEnemyData(_enemy);
-        if(_enemy.PlayerSettings != null)
+        GroundMask = LayerMask.GetMask("Ground");
+        if (_enemy.PlayerSettings != null)
         {
         }else if (_enemy.PlayerSettings == null) { Debug.Log("<color=red>Error: </color> Player was not found.");  }
         if(enemyObj.gameObject.GetComponent<ParticleSystem>() != null)
@@ -142,11 +145,11 @@ public class Enemy : MonoBehaviour
     private void FixedUpdate()
     {
         //Move and change the rotation of the enemy towards the player.
-        if(CheckForPlayer(transform.position, _enemy.FollowRange, _enemy.PlayerObject.GetComponent<Collider>()))
+        if(CheckForPlayer(transform.position, _enemy.FollowRange, _enemy.PlayerObject.GetComponent<Collider>()) || aggro)
         {
             enemyObj.LookAt(lookatvector);
         }
-        if(CheckForPlayer(transform.position, _enemy.FollowRange, _enemy.PlayerObject.GetComponent<Collider>()))
+        if(CheckForPlayer(transform.position, _enemy.FollowRange, _enemy.PlayerObject.GetComponent<Collider>()) || aggro)
         {
             float distanceToPlayer = targetPos.magnitude;
             if (distanceToPlayer > 2f)
@@ -154,9 +157,14 @@ public class Enemy : MonoBehaviour
                 float adjustedSpeed = Mathf.Lerp(0, _enemy.speed, Mathf.Clamp01(distanceToPlayer / _enemy.FollowRange));
 
                 Vector3 move = targetPos.normalized * adjustedSpeed * Time.fixedDeltaTime;
+                if(!isGrounded)
+                {
+                    move.y = -9.81f * 4f * Time.deltaTime;
+                }
                 if (Rigidbody != null)
                 {
                     Rigidbody.MovePosition(transform.position + move);
+                    aggro = true;
                 }
             }
         }
@@ -171,7 +179,9 @@ public class Enemy : MonoBehaviour
 
         LayerMask Enemy = LayerMask.GetMask("Enemy");
         RaycastHit ray;
-        if (Physics.Raycast(enemyObj.position, lookatvector - transform.position, out ray))
+        Vector3 RayPosition = enemyObj.position;
+        RayPosition.y = _enemy.PlayerObject.transform.position.y;
+        if (Physics.Raycast(RayPosition, lookatvector - transform.position, out ray))
         {
             if (ray.collider.CompareTag("Player"))
             {
@@ -179,6 +189,8 @@ public class Enemy : MonoBehaviour
             }
             else PlayerIsInSight = false;
         }
+
+        Debug.DrawRay(RayPosition, lookatvector - transform.position, Color.green);
         //Begin the attack cycle:
 
         playerInRange = CheckForPlayer(transform.position, _enemy.AttackRange, _enemy.PlayerObject.GetComponent<Collider>());
@@ -206,11 +218,9 @@ public class Enemy : MonoBehaviour
             else if(AttackingRange && _enemy.Metronome.IsOnBeat() && CanAttack) { StartCoroutine(StartAttack(boss_enemy.BossPattern[0].pattern)); }
         }
         #region Ground Check + Falling rate
-        if (Physics.Raycast(transform.position, Vector3.down, 2))
-        {
-            isGrounded = true;
-        }
-        if(!isGrounded && Rigidbody != null)
+        Vector3 GroundCheck = transform.GetChild(1).position;
+        isGrounded = Physics.CheckSphere(GroundCheck, 0.4f, GroundMask);
+        if (!isGrounded && Rigidbody != null)
         {
             Rigidbody.drag = 0;
         }
@@ -249,7 +259,7 @@ public class Enemy : MonoBehaviour
     }
     private IEnumerator Charge(float seconds)
     {
-
+        if (_enemy.speed <= 0) _enemy.speed = 13f;
         float begginingspeed = _enemy.speed;
         _enemy.speed = 0f;
         _enemy.ChargeParticle.Play();
@@ -288,7 +298,6 @@ public class Enemy : MonoBehaviour
     {
         LayerMask Player = LayerMask.GetMask("Player");
         LayerMask Enemy = LayerMask.GetMask("Enemy");
-        bool bulletCollided = false;
         GameObject bullet = Instantiate(ranged_enemy.Bullet, Gun.position, Quaternion.identity);
         bool HitPlayer = Physics.CheckSphere(bullet.transform.position, 0.1f, Player);
         Vector3 distance = _enemy.PlayerObject.transform.position - bullet.transform.position;
@@ -311,19 +320,17 @@ public class Enemy : MonoBehaviour
                     PositionOnBeat = transform.position;
                     Debug.Log("The bullet changed direction");
                 }
-                bulletCollided = true;
             }
             else if (bullet != null && Physics.CheckSphere(bullet.transform.position, 0.1f, Enemy))
             {
                 gameObject.GetComponent<EnemyBody>().ModifyHealth(2);
                 Debug.Log("The bullet hit itself");
-                bulletCollided = true;
                 Destroy(bullet);
                 break;
             }
             yield return null;
         }
-        if (bullet != null && !bulletCollided) { Destroy(bullet); Debug.Log("The bullet did not collide with anything"); }
+        if (bullet != null) { Destroy(bullet); Debug.Log("The bullet did not collide with anything"); }
     }
     private void SpinAttack()
     {
@@ -349,10 +356,10 @@ public class Enemy : MonoBehaviour
                         StartCoroutine(Charge(_enemy.ChargeTime));
                         break;
                     case Attack.Light:
-                        LightAttack(_enemy.Damage);
+                        if (PlayerIsInSight == true) LightAttack(_enemy.Damage);
                         break;
                     case Attack.Heavy:
-                        HeavyAttack(_enemy.Damage);
+                        if (PlayerIsInSight == true) HeavyAttack(_enemy.Damage);
                         break;
                     case Attack.Load:
                         StartCoroutine(Load(_enemy.ChargeTime));
@@ -361,10 +368,10 @@ public class Enemy : MonoBehaviour
                         if (PlayerIsInSight == true) StartCoroutine(Shoot(ranged_enemy.BulletDamage));
                         break;
                     case Attack.Spin:
-                        SpinAttack();
+                        if (PlayerIsInSight == true) SpinAttack();
                         break;
                     case Attack.Lag:
-                        EndLag();
+                        if (PlayerIsInSight == true) EndLag();
                         break;
                 }
                 yield return new WaitForSeconds(_enemy.TimeBetweenAttacks);
