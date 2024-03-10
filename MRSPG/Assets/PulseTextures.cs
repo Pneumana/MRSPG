31cbf538d6649ca6f2c19c135fc253ac74709f0e
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-03-10 11:38:55 -0700

    synced pulse materials
    maybe

--- MRSPG/Assets/PulseTextures.cs
+++ MRSPG/Assets/PulseTextures.cs
@@ -6,12 +6,14 @@ public class PulseTextures : MonoBehaviour
 {
     [SerializeField] Material[] materials;
     [SerializeField] int materialFrameSkips;
+    [SerializeField] float delta = 0.02f;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.LogWarning(1 / Time.deltaTime + " FPS");
         foreach(Material mat in materials)
         {
-            mat.SetFloat("_BPMInterval", ((float)Metronome.inst.BPM / 60) - (materialFrameSkips * Time.deltaTime));
+            mat.SetFloat("_BPMInterval", ((float)Metronome.inst.BPM / 60) - (materialFrameSkips * delta));
         }
     }
 
