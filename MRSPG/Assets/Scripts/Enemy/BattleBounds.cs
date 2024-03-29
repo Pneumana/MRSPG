eecd6a5f45137af1fcbb56a14470f54703675462
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-03-28 22:07:54 -0700

    battle oob vfx

--- MRSPG/Assets/Scripts/Enemy/BattleBounds.cs
+++ MRSPG/Assets/Scripts/Enemy/BattleBounds.cs
@@ -9,17 +9,27 @@ public class BattleBounds : MonoBehaviour
     public float boundSize;
     [Tooltip("Add the enemies within the boundary")]
     public List<EnemyBody> enemies = new List<EnemyBody>();
+    [SerializeField]
+    Transform effectiveRange;
 
-    float distance;
-    bool running;
-    bool inBattle;
+    [SerializeField]
+    ParticleSystem damageBuildup, damageBurst;
+
+    public float distance;
+    public bool running;
+    public bool inBattle;
 
     private void Awake()
     {
+        if (player == null)
+            player = GameObject.Find("PlayerObj").transform;
         foreach(EnemyBody enemy in enemies)
         {
             enemy.bounds = this;
         }
+
+        effectiveRange.localScale = new Vector3(boundSize * 2, boundSize * 2, boundSize * 2);
+
     }
 
     private void Update()
@@ -30,11 +40,17 @@ public class BattleBounds : MonoBehaviour
             if(distance < boundSize)
             {
                 inBattle = true;
+                damageBuildup.Stop();
+            }
+            if(inBattle && distance > boundSize)
+            {
+                damageBuildup.transform.position = player.transform.position;
             }
             if(inBattle && !running && distance > boundSize)
             {
                 Debug.Log("The player is leaving the battle, " + distance);
                 StartCoroutine(DrainHP());
+                
             }
         }
     }
@@ -42,10 +58,14 @@ public class BattleBounds : MonoBehaviour
     IEnumerator DrainHP()
     {
         running = true;
+        damageBuildup.Play();
         yield return new WaitForSeconds(2f);
         if(distance > boundSize)
         {
+            damageBurst.transform.position = player.transform.position + Vector3.down;
+            damageBurst.Play();
             player.parent.GetComponent<Health>().LoseHealth(1);
+            //play hurt particles on player
         }
         running = false;
     }
