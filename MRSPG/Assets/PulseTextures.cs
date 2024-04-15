2cb05afc7dfda8d24f846d541010eb884d559724
Pneumana <connor_nicewarner1@my.vcccd.edu>
2024-04-15 15:25:02 -0700

    push!

--- MRSPG/Assets/PulseTextures.cs
+++ MRSPG/Assets/PulseTextures.cs
@@ -7,19 +7,30 @@ public class PulseTextures : MonoBehaviour
     [SerializeField] Material[] materials;
     [SerializeField] int materialFrameSkips;
     [SerializeField] float delta = 0.02f;
+
+    [Range(0,1)]
+    [SerializeField] float eyebleed;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning(1 / Time.deltaTime + " FPS");
-        foreach(Material mat in materials)
-        {
-            mat.SetFloat("_BPMInterval", ((float)Metronome.inst.BPM / 60) - (materialFrameSkips * delta));
-        }
+        Refresh();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    [ContextMenu("Refresh")]
+    public void Refresh()
+    {
+        //Debug.LogWarning(1 / Time.deltaTime + " FPS");
+        //Debug.LogWarning("eyebleed setting is at " + eyebleed);
+        foreach (Material mat in materials)
+        {
+            mat.SetFloat("_BPMInterval", ((float)Metronome.inst.BPM / 60) - (materialFrameSkips * delta));
+            mat.SetFloat("_BPMIntensity", eyebleed);
+        }
     }
 }
